9ccf3d88d7413bf0b475b4f762f3b8892030229f
jkorkalainen <juha.korkalainen@iki.fi>
2015-03-19 10:26:09 +0200

    return type

--- src/main/java/fi/vm/kapa/rova/engine/resources/EngineResource.java
+++ src/main/java/fi/vm/kapa/rova/engine/resources/EngineResource.java
@@ -3,7 +3,9 @@ package fi.vm.kapa.rova.engine.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import fi.vm.kapa.rova.engine.model.Authorization;
 import fi.vm.kapa.rova.engine.model.Delegate;
@@ -13,10 +15,12 @@ public interface EngineResource {
 
 	@GET
 	@Path("/delegate/{personId}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Delegate getDelegate(@PathParam("personId") String personId, @QueryParam("industry") String industry, @QueryParam("service") String service, @QueryParam("issue") String issue);
 
 	@GET
 	@Path("/authorization/{delegateId}/{principalId}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Authorization getAuthorization(@PathParam("delegateId") String delegateId, @PathParam("principalId") String principalId, @QueryParam("industry") String industry, @QueryParam("service") String service, @QueryParam("issue") String issue);
 
 }
