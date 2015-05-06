2a2732a18d2bd8ae015439c6a91d53d2af38c240
jkorkalainen <juha.korkalainen@iki.fi>
2015-05-06 13:01:09 +0300

    added query id

--- src/main/java/fi/vm/kapa/rova/engine/resources/EngineResource.java
+++ src/main/java/fi/vm/kapa/rova/engine/resources/EngineResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -13,11 +14,11 @@ public interface EngineResource {
 	@GET
 	@Path("/delegate/{service}/{enduserId}/{personId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getDelegate(@PathParam("personId") String personId, @PathParam("service") String service, @PathParam("enduserId") String endUserId);
+	public Response getDelegate(@PathParam("personId") String personId, @PathParam("service") String service, @PathParam("enduserId") String endUserId, @QueryParam("queryId") String queryId);
 
 	@GET
 	@Path("/authorization/{service}/{enduserId}/{delegateId}/{principalId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAuthorization(@PathParam("delegateId") String delegateId, @PathParam("principalId") String principalId, @PathParam("service") String service, @PathParam("enduserId") String endUserId);
+	public Response getAuthorization(@PathParam("delegateId") String delegateId, @PathParam("principalId") String principalId, @PathParam("service") String service, @PathParam("enduserId") String endUserId, @QueryParam("queryId") String queryId);
 
 }
