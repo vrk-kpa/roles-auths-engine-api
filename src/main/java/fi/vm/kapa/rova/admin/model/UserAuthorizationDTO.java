1f2a95f089cd67d2ae5e1f835f534c9289dfd1e2
Tuomo Kajava <tuomo.kajava@gofore.com>
2015-08-03 16:37:47 +0300

    user authorization model added

---
+++ src/main/java/fi/vm/kapa/rova/admin/model/UserAuthorizationDTO.java
@@ -0,0 +1,19 @@
+package fi.vm.kapa.rova.admin.model;
+
+public class UserAuthorizationDTO {
+	private String roleName;
+	private String serviceId;
+	
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+}
