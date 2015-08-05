8b89ba6b6d89b94e76e220dba2eda1827635fee9
tkarttu <tkarttu@gmail.com>
2015-08-05 13:58:12 +0300

    KAPAROVA-88 lisatty puuttuva luokka

---
+++ src/main/java/fi/vm/kapa/rova/admin/model/LocalizationDTO.java
@@ -0,0 +1,33 @@
+package fi.vm.kapa.rova.admin.model;
+
+public class LocalizationDTO {
+	
+	private String key;
+	private String lang;
+	private String value;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
