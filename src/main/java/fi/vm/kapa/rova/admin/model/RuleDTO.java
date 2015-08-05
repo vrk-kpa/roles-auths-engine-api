516d14ed0129bca0f009023cf169e1c95493d931
tkarttu <tkarttu@gmail.com>
2015-08-05 13:08:42 +0300

    lisatty localizationKeys-jasenmuuttuja

--- src/main/java/fi/vm/kapa/rova/admin/model/RuleDTO.java
+++ src/main/java/fi/vm/kapa/rova/admin/model/RuleDTO.java
@@ -1,36 +1,55 @@
 package fi.vm.kapa.rova.admin.model;
 
+import java.util.Collection;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RuleDTO {
+	
 	private String ruleId;
 	private String identification;
 	private String description;
 	private String group;
-	
+	private Collection<String> localizationKeys;
+
 	public String getRuleId() {
 		return ruleId;
 	}
+
 	public void setRuleId(String ruleId) {
 		this.ruleId = ruleId;
 	}
+
 	public String getIdentification() {
 		return identification;
 	}
+
 	public void setIdentification(String identification) {
 		this.identification = identification;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getGroup() {
 		return group;
 	}
+
 	public void setGroup(String group) {
 		this.group = group;
+	}
+
+	public Collection<String> getLocalizationKeys() {
+		return localizationKeys;
+	}
+
+	public void setLocalizationKeys(Collection<String> localizationKeys) {
+		this.localizationKeys = localizationKeys;
 	}
 }
