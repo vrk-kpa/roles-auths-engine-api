839390a31273f52c44726911689738aebfa4ad36
Tuomo Kajava <tuomo.kajava@gofore.com>
2015-08-13 11:00:50 +0300

    userDTO

--- src/main/java/fi/vm/kapa/rova/admin/model/UserDTO.java
+++ src/main/java/fi/vm/kapa/rova/admin/model/UserDTO.java
@@ -4,32 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
-    //private int userId;
-    //private String name;
+
     private String userName;
-    
-    /*
-    public int getUserId() {
-        return userId;
-    }
-    
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    */
     
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
