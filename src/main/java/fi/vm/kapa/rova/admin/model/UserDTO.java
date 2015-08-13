9271cbd74658f44714f4bcb5138d68eb6d1238e4
jkorkalainen <juha.korkalainen@iki.fi>
2015-08-13 11:14:20 +0300

    added authorization list

--- src/main/java/fi/vm/kapa/rova/admin/model/UserDTO.java
+++ src/main/java/fi/vm/kapa/rova/admin/model/UserDTO.java
@@ -1,16 +1,25 @@
 package fi.vm.kapa.rova.admin.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private String userName;
+    private List<UserAuthorizationDTO> userAuthorizations;
     
     public String getUserName() {
         return userName;
     }
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+    public List<UserAuthorizationDTO> getUserAuthorizations() {
+        return userAuthorizations;
+    }
+    public void setUserAuthorizations(List<UserAuthorizationDTO> userAuthorizations) {
+        this.userAuthorizations = userAuthorizations;
     }
 }
