a4381ff1243dc8801dcee640400db41fcff4c6df
tkarttu <tkarttu@gmail.com>
2015-08-14 10:55:32 +0300

    lisatty uusi wrapper-luokka vtj-vastauksille

---
+++ src/main/java/fi/vm/kapa/rova/vtj/model/VTJResponse.java
@@ -0,0 +1,33 @@
+package fi.vm.kapa.rova.vtj.model;
+
+public class VTJResponse {
+    private Person person;
+    
+    private boolean success;
+    
+    private String error;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
