0b55a12f9a7498aee25bb20d2fff71223942dffe
tkarttu <tkarttu@gmail.com>
2015-09-08 09:55:51 +0300

    lisatty USER_UNKNOWN-ExceptionType

--- src/main/java/fi/vm/kapa/rova/rest/exception/ExceptionType.java
+++ src/main/java/fi/vm/kapa/rova/rest/exception/ExceptionType.java
@@ -5,6 +5,7 @@ public enum ExceptionType implements ErrorCode {
     MISSING_PARAMETER(101),
     MATCHING_SERVICE_NOT_FOUND(102),
     DUPLICATE_SERVICE_IDENTIFIER(103),
+    USER_UNKNOWN(104),
     OTHER_EXCEPTION(199);
 
     ExceptionType(int number) {
