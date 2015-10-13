e9c5b9d0e4173e2fda320604941ac729019ec16c
Mikko Tomperi <mikko.tomperi@cybercom.com>
2015-10-13 08:04:29 +0300

    added new result type

---
+++ src/main/java/fi/vm/kapa/rova/engine/model/ResultRoleTypeList.java
@@ -0,0 +1,7 @@
+
+package fi.vm.kapa.rova.engine.model;
+
+import java.util.ArrayList;
+
+public class ResultRoleTypeList<ResultRoleType> extends ArrayList<ResultRoleType> implements IResultType {
+}
