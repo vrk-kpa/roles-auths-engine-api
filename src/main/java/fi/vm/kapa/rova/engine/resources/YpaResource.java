29277e918874106eb4628a0c6f34238c49155a14
jkorkalainen <juha.korkalainen@iki.fi>
2015-10-13 09:41:02 +0300

    added organizationIds to parameter

--- src/main/java/fi/vm/kapa/rova/engine/resources/YpaResource.java
+++ src/main/java/fi/vm/kapa/rova/engine/resources/YpaResource.java
@@ -2,11 +2,9 @@ package fi.vm.kapa.rova.engine.resources;
 
 import fi.vm.kapa.rova.engine.model.OrganizationResult;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.Set;
 
 @Path("/rest")
@@ -15,5 +13,6 @@ public interface YpaResource {
     @GET
     @Path("/ypa/roles/{service}/{enduserId}/{personId}")
     @Produces(MediaType.APPLICATION_JSON)
-    Set<OrganizationResult> getRoles(@PathParam("personId") String personId, @PathParam("service") String service);
+    Set<OrganizationResult> getRoles(@PathParam("personId") String personId, @PathParam("service") String service,
+                                     @QueryParam("organizationId") List<String> organizationIds);
 }
