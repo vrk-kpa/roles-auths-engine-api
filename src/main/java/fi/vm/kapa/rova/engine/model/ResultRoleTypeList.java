43be286493b3d2b5628f39462669e85d9d72e2cd
Mikko Tomperi <mikko.tomperi@cybercom.com>
2015-10-21 11:43:48 +0300

    added constructors

--- src/main/java/fi/vm/kapa/rova/engine/model/ResultRoleTypeList.java
+++ src/main/java/fi/vm/kapa/rova/engine/model/ResultRoleTypeList.java
@@ -2,6 +2,16 @@
 package fi.vm.kapa.rova.engine.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class ResultRoleTypeList<ResultRoleType> extends ArrayList<ResultRoleType> implements IResultType {
+    public ResultRoleTypeList() {
+        super();
+    }
+    public ResultRoleTypeList(int size) {
+        super(size);
+    }
+    public ResultRoleTypeList(Collection<ResultRoleType> c) {
+        super(c);
+    }
 }
