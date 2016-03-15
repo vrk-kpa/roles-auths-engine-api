4b515c1db917cc6acb9b0296d1e57afe929fcd36
rjkokko <riku.kokko@gofore.com>
2016-03-15 12:09:49 +0200

    KAPAROVA-536 Service identifier to the the resource paths.

---
+++ src/main/java/fi/vm/kapa/rova/external/model/ServiceIdType.java
@@ -0,0 +1,24 @@
+package fi.vm.kapa.rova.external.model;
+
+public enum ServiceIdType {
+    XROAD("xroad"), WEB_API("webApi"), UUID("uuid");
+    
+    private String text;
+    
+    private ServiceIdType(String text) {
+        this.text = text;
+    }
+    
+    public static ServiceIdType fromText(String text) {
+        for(ServiceIdType type : values()) {
+            if(type.getText().equals(text)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("No such ServiceIdType: " + text);
+    }
+    
+    public String getText() {
+        return text;
+    }
+}
