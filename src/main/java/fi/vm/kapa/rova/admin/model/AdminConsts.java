4268f8b95c3f72dd8e1ead4825f7e68badb2f165
tkarttu <tkarttu@gmail.com>
2016-05-06 11:50:10 +0300

    KAPAROVA-746 pari vakiota lisaa valtuutussaannon konfugurointia varten

--- src/main/java/fi/vm/kapa/rova/admin/model/AdminConsts.java
+++ src/main/java/fi/vm/kapa/rova/admin/model/AdminConsts.java
@@ -2,6 +2,6 @@ package fi.vm.kapa.rova.admin.model;
 
 public class AdminConsts {
     public static final String ADMIN_UI_USER_UUID = "X-Admin-UI-user-uuid";
-
-
+    public static final String RULE_CONFIG_ALLOWED_ISSUES = "allowedissues";
+    public static final String MANDATE_RULE_ID = "019.003.1.1";
 }
