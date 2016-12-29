d856ae028238844640d3681bf040993255a58365
Tommy Savolainen <tommy.savolainen@codemen.fi>
2016-12-29 10:21:28 +0200

    commonista engine-apii:n seuraavat luokat

---
+++ src/main/java/fi/vm/kapa/rova/external/model/ytj/CompanyAuthorizationDataRequest.java
@@ -0,0 +1,13 @@
+package fi.vm.kapa.rova.external.model.ytj;
+
+public class CompanyAuthorizationDataRequest {
+    private String ssn;
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
