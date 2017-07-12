78475c0f092c72bbf1e0466cdc298466071dc566
rjkokko <riku.kokko@gofore.com>
2017-07-12 15:54:13 +0300

    KAPAROVA-1958 Received mandate total and per party.

--- src/main/java/fi/vm/kapa/rova/vare/MandateClient.java
+++ src/main/java/fi/vm/kapa/rova/vare/MandateClient.java
@@ -92,7 +92,7 @@ public interface MandateClient {
     PartiesDTO getPastMandateParties(String partyId, SearchTypeEnum type, List<MandateType> mandateTypes, int limit,
                                      int offset, boolean ascending, PartySortTypeEnum sortBy);
 
-    PartiesDTO getMandateRequestParties(String partyId, SearchTypeEnum type, List<MandateType> mandateTypes, int limit,
+    RequestPartiesDTO getMandateRequestParties(String partyId, SearchTypeEnum type, List<MandateType> mandateTypes, int limit,
                                         int offset, boolean ascending, PartySortTypeEnum sortBy);
 
     PartyDTO getMandateParty(String partyId);
